0875c96d45c4b85a1cf6eec5ddfa733535bf8d2f
Shoshana Rosenfield <Shoshana@MacBook-Air-2.local>
2018-09-24 10:37:37 -0400

    update to include conditional rendering of GS and NYCB resume information

--- main.js
+++ main.js
@@ -1,19 +1,26 @@
-let isActive=false;
-function resumeInfo (){
-
-  if(!isActive){
-    isActive=true;
-    document.getElementById('resumeInfo').style.visibility="visible";
-    console.log('isActive',isActive)
-
-  } else if(isActive){
-    isActive=false
-    document.getElementById('resumeInfo').style.visibility="hidden";
-    console.log('else',isActive)
+function resumeInfo (id){
+  console.log('id',id==='gs')
+  if(id==='NYCB'){
+    if(document.getElementById('nycbInfo').style.visibility==="visible"){
+      document.getElementById('nycbInfo').style.visibility="hidden";
+    }
+    else {
+      // document.getElementsByClassName('resumeDetail').style.visibility = 'hidden'
+      document.getElementById('nycbInfo').style.visibility="visible";
+    }
+  } else if(id==='gs'){
+    // document.getElementsByClassName('resumeInfo').style.visibility="hidden"
+    if(document.getElementById('gsInfo').style.visibility==='hidden'){
+      // document.getElementsByClassName('resumeDetail').style.visibility = 'hidden'
+      console.log('here')
+      document.getElementById('gsInfo').style.visibility='visible'
+    } else {
+      document.getElementById('gsInfo').style.visibility='hidden'
+    }
   }
 }
 
 
-const nycb = document.getElementById('NYCB');
+document.getElementById('NYCB').addEventListener('click', ()=>resumeInfo('NYCB'))
 
-nycb.addEventListener('click', resumeInfo)
+document.getElementById('gs').addEventListener('click', ()=>resumeInfo('gs'))
