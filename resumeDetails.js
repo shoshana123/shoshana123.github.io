f6f77480fe01018e987b329ac0a15e91f569d96e
Shoshana Rosenfield <Shoshana@MacBook-Air-2.local>
2018-09-27 16:13:47 -0400

    update with modular code

--- resumeDetails.js
+++ resumeDetails.js
@@ -1,9 +1,70 @@
-export const resumeDetails = {
+const resumeDetails = {
   gs: {
-    employer: 'Goldman Sachs Co. LLC',
+    employer: 'Goldman Sachs & Co. LLC',
     title: 'Business Analyst',
     date: '2016 - 2018',
     location: 'New York, NY',
-    description: 'Streamlined, analyzed and produced monthly reports and presentations including business’ P&L, net sales, product pipeline, headcount, client uptake and regulatory data requests\nCoded dynamically generated client documents for European regulation based on investment performance in 4 languages\nMonitored the non-compensation expenses for 4 regions and 5 content specialist areas of the business'
+    description: 'Streamlined, analyzed and produced monthly reports and presentations including business’ P&L, net sales, product pipeline, headcount, client uptake and regulatory data requests <br> Coded dynamically generated client documents for European regulation based on investment performance in 4 languages <br> Monitored the non-compensation expenses for 4 regions and 5 content specialist areas of the business'
+  },
+  NYCB: {
+    employer: 'New York City Ballet',
+    title: 'Corps de Ballet',
+    date: '2009 - 2012',
+    location: 'New York, NY',
+    description: 'Performed in 7 - 10 shows weekly, 6 days per week and rehearsed 6 - 8 hours daily'
+  },
+  barnard: {
+    employer:'Barnard College, Columbia University',
+    title: 'Bachelor of Arts in Economics',
+    date: '2012 - 2015',
+    location: 'New York, NY',
+    description: 'Coursework included: Microeconomics, Macroeconomics, Calculus I, Statistics, Financial Economics, Political Economy'
+
+  },
+  graceHopper: {
+    employer:'Grace Hopper Program at Fullstack Academy',
+    title: 'Teaching Fellow',
+    date: '2018 - Present',
+    location: 'New York, NY',
+    description: 'Debugged, mentored and taught full-stack JavaScript concepts and best practices to 40+ students<br>Administered technical interviews to prospective students<br>Curriculum focused on full-stack web development using JavaScript, with topics including Node, Express, React, Redux and PostgreSQL using Sequelize ORM'
   }
+
 };
+
+
+
+
+
+
+
+// {
+//   "gs": {
+//     "employer": "Goldman Sachs & Co. LLC",
+//     "title": "Business Analyst",
+//     "date": "2016 - 2018",
+//     "location": "New York, NY",
+//     "description": "Streamlined, analyzed and produced monthly reports and presentations including business’ P&L, net sales, product pipeline, headcount, client uptake and regulatory data requests <br> Coded dynamically generated client documents for European regulation based on investment performance in 4 languages <br> Monitored the non-compensation expenses for 4 regions and 5 content specialist areas of the business"
+//   },
+//   "NYCB": {
+//     "employer": "New York City Ballet",
+//     "title": "Corps de Ballet",
+//     "date": "2009 - 2012",
+//     "location": "New York, NY",
+//     "description": "Performed in 7 - 10 shows weekly, 6 days per week and rehearsed 6 - 8 hours daily"
+//   },
+//   "barnard": {
+//     "employer":"Barnard College, Columbia University",
+//     "title": "Bachelor of Arts in Economics",
+//     "date": "2012 - 2015",
+//     "location": "New York, NY",
+//     "description": "Coursework included: Microeconomics, Macroeconomics, Calculus I, Statistics, Financial Economics, Political Economy"
+
+//   },
+//   "graceHopper": {
+//     "employer":"Grace Hopper Program at Fullstack Academy",
+//     "title": "Teaching Fellow",
+//     "date": "2018 - Present",
+//     "location": "New York, NY",
+//     "description": "Debugged, mentored and taught full-stack JavaScript concepts and best practices to 40+ students<br>Administered technical interviews to prospective students<br>Curriculum focused on full-stack web development using JavaScript, with topics including Node, Express, React, Redux and PostgreSQL using Sequelize ORM"
+//   }
+// }
